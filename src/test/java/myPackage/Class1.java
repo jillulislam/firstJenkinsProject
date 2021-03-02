ea0acb464e42bf9491014f920e74b09b77317e05
Jillul Islam <jillul.islam@outlook.com>
2021-03-02 19:25:02 +0000

    first commit

---
+++ src/test/java/myPackage/Class1.java
@@ -0,0 +1,15 @@
+package myPackage;
+
+import org.testng.annotations.Test;
+
+public class Class1 {
+
+    @Test
+    public void myName(){
+        System.out.println("Ahiya");
+    }
+    @Test
+    public void myAge(){
+        System.out.println("38 years");
+    }
+}
---
+++ src/test/java/myPackage/Class2.java
@@ -0,0 +1,15 @@
+package myPackage;
+
+import org.testng.annotations.Test;
+
+public class Class2 {
+
+    @Test
+    public void myAddress(){
+        System.out.println("4 Chipka Street");
+    }
+    @Test
+    public void myNumber(){
+        System.out.println("079877786767");
+    }
+}
